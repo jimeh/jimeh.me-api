15ff4ba6e6aeb9813e23942b49156cb2d79ebcd0
Jim Myhrberg <contact@jimeh.me>
2011-10-31 12:55:56 +0000

    silly mistake :P

--- app/models/post.rb
+++ app/models/post.rb
@@ -74,7 +74,7 @@ class Post < Hash
   end
 
   def uid
-    @slug ||= filename.match(/^(?:\d{4}-\d{2}-\d{2}-|)(.+)\.md$/)[1]
+    @uid ||= filename.match(/^(?:\d{4}-\d{2}-\d{2}-|)(.+)\.md$/)[1]
   end
 
   private
